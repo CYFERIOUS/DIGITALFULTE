f2ed130e1dad9dc1c4d8ee9b74524d051fccf96d
CYFERIOUS <frankdrujes@gmail.com>
2018-02-04 19:07:16 -0500

    3d portfolio on the way

---
+++ sessions/3d/inD3x.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>3D PORTFOLIO</title>
+
+<script src="../../bower_components/jquery/dist/jquery.min.js" type="text/javascript"></script>
+<script src="../../bower_components/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+
+<script src="js/filter.js" type="text/javascript"></script>
+	<script src="js/scriptos.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+</head>
+
+<body>
+<div id="header">
+<div id="menu">
+	<h2>DIGITAL FLUTE 3D PORTFOLIO</h2>
+    <input id="search" style="float:right"type="text" placeholder="search"/>
+</div>
+</div>
+<div id="overlay"></div>
+
+<div id="frame">
+ 
+	<img id="main" src="" alt="img"/> 
+  
+    <table id="frame-arrows">
+	
+    <tr>
+		<td id="left">
+        	<img src="images/flechas2.png" alt="left"/>
+        </td>
+        <td><div id="description">
+    <p>  </p>
+    </div></td>
+		<td id="right">
+        	<img src="images/flechas.png" alt="right"/>
+        </td>
+	</tr>
+   
+</table>
+
+</div>
+
+<div id="wraper">
+<ul id="filter">
+	<li class="active">ALL</li>
+	<li>CYBERPUNK</li>
+	<li>HORROR</li>
+    <li>KIDS</li>
+    <li>ONIRICAL</li>
+</ul>
+<ul id="portfolio">
+	<?php include_once("list.html") ?>
+</ul>
+</div>
+</body>
+</html>
---
+++ sessions/3d/js/scriptos.js
@@ -0,0 +1,93 @@
+// JavaScript Document
+// JavaScript Document
+$(function(){
+	
+	var current_li;
+	
+	$("#search").keyup(function(){
+		
+		var query = $(this).val();
+		
+		if(query != ""){
+		$("#portfolio li").hide();
+		$("#portfolio li").each(function() {
+            var current = $(this).attr("data-keywords");
+			if(current.indexOf(query)>=0){
+				$(this).show();
+			}
+        });
+		}else{
+			$("#portfolio li").show();
+		}
+		
+	});
+	
+	////sort images with ajaxs post method. 
+	$("#portfolio").sortable({stop:
+		function(){
+		var list_cont = $("#portfolio").html();
+		$.post("charge.php",{list:list_cont});
+		}
+		});
+		
+	/////shoot images into main div.	
+	function getIMG(src,id){
+		$("#main").attr("src",src);
+		var path = "texto/"+id+".txt";
+		$.get(path,function(data){
+			console.log(data);
+			$("#description p").html(data);
+		});
+	}
+	
+	
+	
+	////active the greybox every time you click on image
+	
+	$("#portfolio img").click(function(){
+	
+		var src = $(this).attr("src");
+		var id = $(this).attr("id");
+		current_li = $(this).parent();
+		getIMG(src,id);
+		$("#frame").fadeIn();
+		$("#overlay").fadeIn();
+	
+	});
+	
+	$("#overlay").click(function(){
+		$(this).fadeOut();
+		$("#frame").fadeOut();
+		
+	});
+	
+	$("#right").click(function(){
+	
+		if(current_li.is(":last-child")){
+			var next_li = $("#portfolio li").first();
+		}else{
+			  var next_li = current_li.next();
+		}
+		
+		var next_src = next_li.children("img").attr("src");
+		var id = next_li.children("img").attr("id");
+		getIMG(next_src,id);
+		current_li = next_li;
+	});
+	
+	$("#left").click(function(){
+		
+		if(current_li.is(":first-child")){
+			var prev_li = $("#portfolio li").last();
+		}else{
+			var prev_li = current_li.prev();
+		}
+		
+		
+		var prev_src = prev_li.children("img").attr("src");
+		var id = prev_li.children("img").attr("id");
+		getIMG(prev_src,id);
+		current_li = prev_li;
+	});
+	
+});
