66fba494f9a9d641cee752595d05747a47f68363
CYFERIOUS <frankdrujes@gmail.com>
2018-03-04 20:33:45 -0500

    working on menu scss for mixin, im tired man

--- sessions/contact.php
+++ sessions/contact.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>dgflute</title>
 
-<link href="../css/contactResp.css" rel="stylesheet" type="text/css" />
+<link href="../css/flutest.css" rel="stylesheet" type="text/css" />
 
 </head>
 
