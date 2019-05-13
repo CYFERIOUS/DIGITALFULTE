2e69abda61a4e8f276d11cd475290ace8ef03cdb
CYFERIOUS <frankdrujes@gmail.com>
2019-05-12 21:14:54 -0500

    portfolio updated for chino popo

--- gulpfile.js
+++ gulpfile.js
@@ -19,6 +19,12 @@ gulp.task('sassIlustre', function() {
         .pipe(gulp.dest('sessions/ilustre/ferchoFolio/css/'));
 });
 
+gulp.task('chinoPopo', function() {
+    return gulp.src('scss/ilustreDevices.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('sessions/ilustre/kamusFolio/css/'));
+});
+
 gulp.task('sassAcordion', function() {
     return gulp.src('scss/SpryAccordion.scss')
         .pipe(sass())
@@ -28,8 +34,8 @@ gulp.task('sassAcordion', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('scss/*.scss','sessions/ilustre/ferchoFolio/scss/*.scss'['sass','sassAcordion','sassIlustre']);
+    gulp.watch('scss/*.scss','sessions/ilustre/ferchoFolio/scss/*.scss'['sass','sassAcordion','sassIlustre','chinoPopo']);
 });
 
 // Default Task
-gulp.task('default', [ 'sass','watch','sassAcordion','sassIlustre']);
+gulp.task('default', [ 'sass','watch','sassAcordion','sassIlustre','chinoPopo']);
