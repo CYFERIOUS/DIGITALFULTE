b609e50ac79fcc8c7f1c77f7b27c5b7e622b5e19
CYFERIOUS <frankdrujes@gmail.com>
2019-05-12 20:08:53 -0500

    styles for illustration portfolio mixed like webpage pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp'); 
 
 // Include Our Plugins
+
 var sass = require('gulp-sass');
 
 // Compile Our Sass
@@ -10,6 +11,12 @@ gulp.task('sass', function() {
     return gulp.src('scss/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('css'));
+});
+
+gulp.task('sassIlustre', function() {
+    return gulp.src('scss/ilustreDevices.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('sessions/ilustre/ferchoFolio/css/'));
 });
 
 gulp.task('sassAcordion', function() {
@@ -21,8 +28,8 @@ gulp.task('sassAcordion', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass','sassAcordion']);
+    gulp.watch('scss/*.scss','sessions/ilustre/ferchoFolio/scss/*.scss'['sass','sassAcordion','sassIlustre']);
 });
 
 // Default Task
-gulp.task('default', [ 'sass','watch','sassAcordion']);
+gulp.task('default', [ 'sass','watch','sassAcordion','sassIlustre']);
